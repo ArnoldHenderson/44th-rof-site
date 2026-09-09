e18aad2f0f2eb22e2b0520e8434591a29a6c1efd
ArnoldHenderson <dj.ender.red.345@gmail.com>
2026-09-09 14:35:12 +0100

    Updated next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/44th-rof-site",
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
